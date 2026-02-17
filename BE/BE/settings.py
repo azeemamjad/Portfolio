59f2f8e0ab80419c63833f33fc909a997d1cd2ad
azeemamjad <azeemamjad225@gmail.com>
2026-02-17 09:56:51 +0500

    added some confs

--- BE/BE/settings.py
+++ BE/BE/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-onnxgw1(ksfw)&63sszg0j8_th)_tvp^+^%v$jyz1i3sx&6c33
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["http://72.62.175.219"]
 
 
 # Application definition
@@ -140,6 +140,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:5173",
     "http://127.0.0.1:3000",
+    "http://72.62.175.219:3000"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
