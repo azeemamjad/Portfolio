50f08e99ae5471452975ba1a5a40fecabd6d5692
azeemamjad <azeemamjad225@gmail.com>
2026-02-25 18:42:18 +0500

    corrected domains

--- FE/vite.config.ts
+++ FE/vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',        // allow external access
+    port: 3000,
+    allowedHosts: ['dev-link.cloud', 'www.dev-link.cloud'],
+  }
 })
