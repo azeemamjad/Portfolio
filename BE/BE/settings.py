db222d88f44a446da3a23a9a99c4677a704ad6f3
azeemamjad <azeemamjad225@gmail.com>
2026-05-14 17:27:39 +0500

    corrected allowed hosts

--- BE/BE/settings.py
+++ BE/BE/settings.py
@@ -140,13 +140,15 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:5173",
     "http://127.0.0.1:3000",
-    "http://backend.dev-link.cloud",
-    "https://backend.dev-link.cloud"
+    "http://dev-link.cloud",
+    "https://dev-link.cloud",
+    "http://www.dev-link.cloud",
+    "https://www.dev-link.cloud"
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    "http://backend.dev-link.cloud",
-    "https://backend.dev-link.cloud",
+    "http://dev-link.cloud",
+    "https://dev-link.cloud",
     "http://www.dev-link.cloud",
     "https://www.dev-link.cloud"
 ]
